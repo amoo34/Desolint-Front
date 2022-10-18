9b8069a381c4cd8dc60fea6fe786cbd2a22b9522
amoo34 <a.moizfaisal123@gmail.com>
2022-10-18 15:33:34 +0500

    update build

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,10 @@
 import { useRoutes } from "react-router-dom";
 import Signin from "./modules/auth/pages/Signin";
-// import Signup from './modules/auth/pages/Signup'
-// import Dashboard from './modules/Dashboard/pages/Dashboard'
 import CreateCar from "./modules/car/pages/CreateCar";
-// import WorkerLayout from './modules/common/components/WorkerLayout'
 
 // ----------------------------------------------------------------------
 
 export default function Router() {
-  //   let newAccess_Token = null;
-  //   newAccess_Token = localStorage.getItem('access_token');
-  //   console.log("SSSSSSSSSSSSSSSSSSSS",newAccess_Token)
   return useRoutes([
     {
       path: "/car",
